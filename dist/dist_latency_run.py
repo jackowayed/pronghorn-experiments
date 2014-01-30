73f4f9c34bc281a7dbbb9c163ebf39a66ab963bd
behram mistree <bmistree@gmail.com>
2014-01-29 17:06:06 -0800

    Filled in more methods for experimental setup: added in stop, etc.

--- dist/dist_latency_run.py
+++ dist/dist_latency_run.py
@@ -4,33 +4,178 @@ import subprocess
 import time
 
 
-EC2_HOSTS = []
+
 FLOODLIGHT_REST_PORT = 8080
 LISTENING_FOR_PARENT_ON_PORT = 3939
 PEM_FILENAME = 'sergeant3.pem'
 
 EXPERIMENT_BUILD_PATH = 'pronghorn/src/experiments/pronghorn/build'
 
+
+EC2_HOSTS = []
+
 # wait 5 seconds between starting a sergeant instance and its parent.
 WAIT_TIME_BETWEEN_SERGEANT_STARTS = 5 
 # used as a general barrier when one operation depends on another
 GENERAL_WAIT_TIME = 20
 
+FAST_WAIT_TIME = 5
+
 # # Example usage
 # def run_test_setup():
 #     hosts = ['ec2-54-184-51-225.us-west-2.compute.amazonaws.com']
 #     # start mininet
-#     start_mininet_and_floodlight(hosts,5)
-#     # run dumb latency test
-#     dumb_latency_test(hosts)
+#     # start_mininet_and_floodlight(hosts,5)
+#     # # run dumb latency test
+#     # dumb_latency_test(hosts)
 #     # stop mininet and floodlight
 #     # stop_mininet_and_floodlight(hosts)
+#     collect_data (
+#         hosts[0],EXPERIMENT_BUILD_PATH+'/classes/latency_out.csv',
+#         'latency_out.csv')
 
-# def run_test_setup():
-#     pass
+LATENCY_TEST_OUTPUT_FILENAME = 'dist_latency_test2.csv'
+LATENCY_NUM_OPS = 1000
+DIST_LATENCY_TEST_NAME = 'run_MultiControllerLatency'
+
+
+def run_test_setup():
+    hosts = [
+        'ec2-54-203-86-171.us-west-2.compute.amazonaws.com',
+        'ec2-54-202-252-75.us-west-2.compute.amazonaws.com']
+    run_latency_test(hosts,'latency_results.csv')
+    # stop_mininet_and_floodlight(hosts)
+    # stop_sergeants(hosts,DIST_LATENCY_TEST_NAME)
+    print '\nWaiting a bit\n'
+    time.sleep(FAST_WAIT_TIME)
+
+    
+def chmod_all_pronghorn (hosts):
+    ssh_cmd_str = 'sudo chmod a=wrx -R pronghorn/'
+    for host in hosts:
+        issue_ssh(host,ssh_cmd_str)
+    
+    
+############### LATENCY EXPERIMENT CONSTANTS ###############
+def run_latency_test(hosts,local_output_file):
+    print '\nchmoding all\n'
+    chmod_all_pronghorn (hosts)
+    
+    # actually bring up mininet and floodlight on each host
+    start_mininet_and_floodlight(hosts,1)
+
+    arguments = (
+        ' -Dlatency_port_to_listen_for_connections_on=%i ' %
+        LISTENING_FOR_PARENT_ON_PORT)
+
+    # tell head nodes to collect data and collect a particular number
+    # of ops.
+    head_arguments = ' -Doutput_filename=%s ' % LATENCY_TEST_OUTPUT_FILENAME
+    head_arguments += ' -Dlatency_num_ops=%i ' % LATENCY_NUM_OPS
+
+    # tell non-head nodes to do nothing
+    non_head_arguments = ' -Dlatency_num_ops=0 ' 
+
+
+    # parents can connect to their children on this ip/port
+    point_at_child_arg_name = ' -Dlatency_children_to_contact_host_ports='
+
+    # start the latency tests in a chained topology
+    print '\nCreating chained sergeants\n'
+    start_sergeants_chained(
+        hosts,DIST_LATENCY_TEST_NAME,head_arguments,non_head_arguments,
+        point_at_child_arg_name)
+    
+    time.sleep(GENERAL_WAIT_TIME*10)
+
+    print '\nCollecting data\n'
+    data_filename_on_server = (
+        EXPERIMENT_BUILD_PATH + '/classes/' + LATENCY_TEST_OUTPUT_FILENAME)
+    collect_data(
+        hosts[0],data_filename_on_server, local_output_file)
+
+    
+
+def dumb_latency_test(hosts):
+    '''
+    Just run single controller latency test on each host
+    '''
+    output_filename = 'latency_out.csv'
+    ssh_cmd = (
+        'cd ' + EXPERIMENT_BUILD_PATH + '; ' + 
+        'ant run_SingleControllerLatency -Dlatency_num_ops=100 -Doutput_filename=' +
+        output_filename)
+    for host in hosts:
+        issue_ssh(host,ssh_cmd)
+
+    time.sleep(GENERAL_WAIT_TIME)
+
+        
+    
+
+def start_sergeants_chained(
+    hosts,test_to_run,head_arguments,non_head_arguments,point_at_child_arg_name):
+    '''
+    @param {List} hosts --- Each element is a hostname string.  The
+    0th entry of the list is the root.  Ie, hosts[0] is the parent for
+    hosts[1], hosts[1] is the parent for hosts[2], ...
+
+    @param {List} head_arguments --- The arguments that we should pass
+    in to the head node.  Each element is a string.
+
+    @param {List} non_head_arguments --- The arguments that we should
+    pass to all non-head nodes.  Each element is a string.
+    '''
+    # the first entry in hosts is the root
+    for reverse_index in reversed(range(0,len(hosts))):
+        host = hosts[reverse_index]
+
+        arguments_to_use = non_head_arguments
+        if reverse_index == 0:
+            arguments_to_use = head_arguments
+
+        if reverse_index != (len(hosts) -1):
+            # we are a parent of a child: must explicitly connect to
+            # child.
+            child_host = hosts[reverse_index + 1]
+            arguments_to_use += (
+                ' %s=%s:%i' %
+                ( point_at_child_arg_name, child_host,LISTENING_FOR_PARENT_ON_PORT))
+            
+        ant_command = 'ant ' + test_to_run + ' ' + arguments_to_use
+        issue_start_sergeant(host,ant_command)
+        time.sleep(WAIT_TIME_BETWEEN_SERGEANT_STARTS)
+
+        
+def issue_start_sergeant(host_to_start,test_name_and_arguments):
+    '''
+    @param {String} host_to_start --- Start a sergeant instance on
+    this host.
+    
+    @param {String or None} child_host --- Tell the sergeant instance
+    on host host_to_start to connect, as a parent, to running sergeant
+    instance on child_host.  If child_host is None, do not connect to
+    any child.
+
+    @param {String} additional_arguments --- Each element is a string
+    containing an argument we want to pass in when starting the node.
+    '''
+    issue_ssh(
+        host_to_start,
+        'cd %s; %s' % (EXPERIMENT_BUILD_PATH, test_name_and_arguments))
+    
+    
+def stop_sergeants(hosts,what_to_pgkill_with):
+    '''
+    @param {List} hosts --- Each element is a string host name.
+    '''
+    for host in hosts:
+        cmd_str = 'sudo pkill -f %s' % what_to_pgkill_with
+        issue_ssh(host,cmd_str)
 
 def start_mininet_and_floodlight(hosts,num_switches):
     # compile floodlight on all hosts
+    print '\nCompiling floodlight\n'
     ssh_cmd = 'cd floodlight; ant'
     for host in hosts:
         issue_ssh(host,ssh_cmd)
@@ -55,83 +200,25 @@ def start_mininet_and_floodlight(hosts,num_switches):
     time.sleep(GENERAL_WAIT_TIME)
 
 
-    
-def dumb_latency_test(hosts):
-    '''
-    Just run single controller latency test on each host
-    '''
-    output_filename = 'latency_out.csv'
-    ssh_cmd = (
-        'cd ' + EXPERIMENT_BUILD_PATH + '; ' + 
-        'sudo ant run_SingleControllerLatency -Dlatency_num_ops=100 -Doutput_filename=' +
-        output_filename)
-    for host in hosts:
-        issue_ssh(host,ssh_cmd)
-
-    time.sleep(GENERAL_WAIT_TIME)
-
-        
-    
 def issue_ssh(host,ssh_cmd_str):
     '''
     @param {String} host --- name of host we're ssh-ing to 
     '''
-    cmd = ['ssh','-i',PEM_FILENAME,'ubuntu@%s' % host,ssh_cmd_str]
+    cmd = ['ssh','-i',PEM_FILENAME,
+           '-o','StrictHostKeyChecking=no',
+           'ubuntu@%s' % host,ssh_cmd_str]
     subprocess.Popen(cmd)
-    
-    
 
-def start_sergeants_chained(hosts,head_arguments,non_head_arguments):
-    '''
-    @param {List} hosts --- Each element is a hostname string.  The
-    0th entry of the list is the root.  Ie, hosts[0] is the parent for
-    hosts[1], hosts[1] is the parent for hosts[2], ...
-
-    @param {List} head_arguments --- The arguments that we should pass
-    in to the head node.  Each element is a string.
-
-    @param {List} non_head_arguments --- The arguments that we should
-    pass to all non-head nodes.  Each element is a string.
-    '''
-    # the first entry in hosts is the root
-
-    for reverse_index in reversed(range(0,len(hosts))):
-        host = hosts[reverse_index]
-        child_host = None
-        if reverse_index != (len(hosts) -1):
-            child_host = hosts[reverse_index + 1]
-
-        arguments_to_use = non_head_arguments
-        if reverse_index == 0:
-            arguments_to_use = head_arguments
-            
-        issue_start_sergeant(host,child_host,arguments_to_use)
-        time.sleep(WAIT_TIME_BETWEEN_SERGEANT_STARTS)
-
-        
-def issue_start_sergeant(host_to_start,child_host,additional_arguments):
-    '''
-    @param {String} host_to_start --- Start a sergeant instance on
-    this host.
-    
-    @param {String or None} child_host --- Tell the sergeant instance
-    on host host_to_start to connect, as a parent, to running sergeant
-    instance on child_host.  If child_host is None, do not connect to
-    any child.
-
-    @param {List} additional_arguments --- Each element is a string
-    containing an argument we want to pass in when starting the node.
-    '''
+def collect_data(host_to_collect_from,absolute_data_filename,local_name):
     # TODO
+    cmd_vec = [
+        'scp','-i',PEM_FILENAME,
+        'ubuntu@%s:%s'% (host_to_collect_from, absolute_data_filename),
+        local_name]
+    p = subprocess.Popen(cmd_vec)
+    p.wait()
     
-    
-def stop_sergeants(hosts):
-    '''
-    @param {List} hosts --- Each element is a string host name.
-    '''
-    for host in hosts:
-        issue_stop_sergeant(host)
-
+        
 
 def stop_mininet_and_floodlight(hosts):
     for host in hosts:
@@ -141,14 +228,7 @@ def stop_mininet_and_floodlight(hosts):
         
         # kill floodlight
         cmd_str = 'sudo pkill -f floodlight'
-        issue_ssh(host,cmd_str)        
-
-        
-
-def collect_data(host_to_collect_from,absolute_data_filename):
-    # TODO
-    pass
-    
+        issue_ssh(host,cmd_str)
 
 
 if __name__ == '__main__':
