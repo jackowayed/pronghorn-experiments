96c97c97efb178afb772c3547657820031818341
behram mistree <bmistree@gmail.com>
2014-02-18 18:32:19 -0800

    Changed arguments to distributed latency to allow user to specify whether running tree or linear topo.

--- dist/dist_latency_run.py
+++ dist/dist_latency_run.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 
-from dist_util import read_conf_file, kill_all, run_linear_test
 import time
 import sys
+import argparse
+
+from dist_util import read_conf_file, kill_all, run_linear_test
+from dist_util import run_tree_test
+
 
 LATENCY_TEST_JAR_NAME = 'multi_controller_latency.jar'
 
 DEFAULT_NUM_OPS_TO_RUN = 30000
 
-MAX_EXPERIMENT_WAIT_TIME_SECONDS = 80
+MAX_EXPERIMENT_WAIT_TIME_SECONDS = 60
 
 def linear_latency_test(local_filename_to_save_to,
                         num_ops_per_switch=DEFAULT_NUM_OPS_TO_RUN):
-
     run_linear_test(
         LATENCY_TEST_JAR_NAME,
         local_filename_to_save_to,
@@ -21,31 +24,65 @@ def linear_latency_test(local_filename_to_save_to,
         MAX_EXPERIMENT_WAIT_TIME_SECONDS,
         1)
 
+def tree_latency_test(local_filename_to_save_to,
+                      num_ops_per_switch=DEFAULT_NUM_OPS_TO_RUN):
+    run_tree_test(
+        LATENCY_TEST_JAR_NAME,
+        local_filename_to_save_to,
+        num_ops_per_switch,
+        'java -jar %s %s %i %i %s',
+        MAX_EXPERIMENT_WAIT_TIME_SECONDS,
+        1)
+
+    
 def kill_latency_experiments(host_entry_list=None):
     if host_entry_list is None:
         host_entry_list = read_conf_file()
     kill_all(host_entry_list,LATENCY_TEST_JAR_NAME)
 
 
-def print_usage():
-    print ('''
+def run_cli():    
+    description_string = '''
+Use this script to run distributed latency experiments on ec2 nodes.
+'''
+    parser = argparse.ArgumentParser(description=description_string)
+    parser.add_argument(
+        '--kill',action='store_true',
+        help='Destroy all running latency tests and mininets')
+    parser.add_argument(
+        '--topo',choices=['linear','tree'],
+        help='Run the latency experiment in a linear or tree topology.')
+    parser.add_argument(
+        '--out',
+        help='Name of file to save results to locally.')
 
-    ./dist_latency_run <arg>
-
-        <arg> --- Either -kill, to destroy all latency tests and
-        partially running latency tests on all hosts or name of file to
-        save results to locally.
-
-''')
+    args = parser.parse_args()
     
-        
-if __name__ == '__main__':
 
-    if len(sys.argv) != 2:
-        print_usage()
+    if args.kill:
+        kill_latency_experiments()
     else:
-        if sys.argv[1] == '-kill':
-            kill_latency_experiments()
+        output_filename = args.out
+        if output_filename is None:
+            print '\nError: if not killing, require output filename.\n'
+            return
+
+        topo = args.topo
+        if topo is None:
+            print '\nError: if not killing, require topology.\n'
+            return
+
+        if topo == 'linear':
+            linear_latency_test(output_filename)
+        elif topo == 'tree':
+            tree_latency_test(output_filename)
+        #### DEBUG
         else:
-            linear_latency_test(sys.argv[1])
+            print ('\nUnexpected topo type\n')
+            assert(False)
+        #### END DEBUG
     
+
+    
+if __name__ == '__main__':
+    run_cli()
